f3df19ec96721dd01b846d927161a4a82d2e581d
Myaroshev <addagon29@gmail.com>
2020-04-22 21:45:37 +0500

    Clicker v0.2

--- clicker.py
+++ clicker.py
@@ -2,37 +2,30 @@ from tkinter import *
 from tkinter import Tk, Button
 from PIL import Image, ImageTk
 import os
-expression = "" 
-def press(num): 
-    global expression 
-    expression = expression + str(num) 
-    equation.set(expression) 
-def ravno(): 
-    try: 
-        global expression 
-        total = str(eval(expression)) 
-        equation.set(total) 
-        expression = "" 
-    except: 
-        equation.set("Ошибочка") 
-        expression = "" 
-def clear(): 
-    global expression 
-    expression = "" 
-    equation.set("") 
-root = Tk()
-equation = StringVar()
-equation.set('Количество кликов: ')
-expression_field = Entry(root, textvariable=equation) 
-expression_field.grid(columnspan=4, ipadx=70) 
-root.title("IEM Кликер")
-root.geometry("320x300")
-def create_window():
-    window = Toplevel(root)
-    image = ImageTk.PhotoImage(Image.open("C:/a/ZBWoVQuQytQ.jpg"))
-    l = Label(window, image=image)
-    l.image=image
-    l.pack()
-image = ImageTk.PhotoImage(Image.open("M:/Git/IEM2/homa.jpg"))
-Button(root, image=image, command=create_window).grid(columnspan=1)
-root.mainloop()
+import time
+
+
+from tkinter import * 
+
+clicks = 0 
+
+
+def click_button(): 
+    global clicks 
+    clicks += 1 
+    game.title("Количество кликов: {}".format(clicks)) 
+    
+clicker = Tk() 
+game = Tk()
+
+clicker.title("Кликер от IEM") 
+game.title("Количество кликов: 0") 
+clicker.geometry("300x250") 
+game.geometry("315x0")
+
+
+btn = Button(text="Click Me", background="#555", foreground="#ccc", 
+padx="20", pady="8", font="16", command=click_button) 
+btn.pack() 
+
+clicker.mainloop()
